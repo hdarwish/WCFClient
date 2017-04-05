d3e620bec64fe3dd31e0b730c25ab69df9b1246c
hdarwish <hafs.darwish@gmail.com>
2017-04-05 12:46:57 +0200

    wcf client

---
+++ WCFClientApplication/WebForm1.aspx
@@ -0,0 +1,96 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WCFClientApplication.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        
+       
+        <table>
+            <tr>
+                <td>
+                     <asp:Label ID="Label1" runat="server" Text="ID"></asp:Label> 
+                </td>
+                 <td>
+                     <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+          <tr>
+                <td>
+                     <asp:Label ID="Label2" runat="server" Text="Name"></asp:Label> 
+                </td>
+                 <td>
+                     <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                     <asp:Label ID="Label3" runat="server" Text="Gender"></asp:Label> 
+                </td>
+                 <td>
+                     <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                     <asp:Label ID="Label4" runat="server" Text="Date of Birth"></asp:Label> 
+                </td>
+                 <td>
+                     <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                     <asp:Label ID="Label6" runat="server" Text="Employee Type"></asp:Label> 
+                </td>
+                <td>
+                    <asp:DropDownList ID="ddlEmployeeType" runat="server" OnSelectedIndexChanged="ddlEmployeeType_SelectedIndexChanged" AutoPostBack="True">
+                        <asp:ListItem Value="-1" Text="Select Employee Type"></asp:ListItem>
+                        <asp:ListItem Value="1" Text="Full Time Employee"></asp:ListItem>
+                        <asp:ListItem Value="2" Text="Part Time Employee"></asp:ListItem>
+                    </asp:DropDownList>
+
+                </td>
+            </tr>
+            <tr id="trAnnualsalary" runat="server" visible="false">
+                <td>
+                     <asp:Label ID="Label7" runat="server" Text="Annual Salary"></asp:Label> 
+                </td>
+                <td>  <asp:TextBox ID="txtannual" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr  id="trhrpay" runat="server" visible="false">
+                 <td>
+                     <asp:Label ID="Label8" runat="server" Text="Hourly Pay"></asp:Label> 
+                </td>
+                <td>  <asp:TextBox ID="txthrpay" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr  id="trhourworked" runat="server" visible="false">
+                 <td>
+                     <asp:Label ID="Label9" runat="server" Text="Hours Worked"></asp:Label> 
+                </td>
+                <td>  <asp:TextBox ID="txthourworked" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td >
+                    <asp:Button ID="btnGetEmp" runat="server" Text="Get Employee" OnClick="btnGetEmp_Click" />
+                </td>
+                <td >
+                    <asp:Button ID="btnSaveEmp" runat="server" Text="Save Employee" OnClick="btnSaveEmp_Click" />
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <asp:Label ID="Label5" runat="server" Text="Labelmsg" />
+                </td>
+            </tr>
+        </table>
+       
+    </div>
+    </form>
+</body>
+</html>
---
+++ WCFClientApplication/WebForm1.aspx.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WCFClientApplication
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnGetEmp_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                EmployeeService.IEmployeeService client =
+                   new EmployeeService.EmployeeServiceClient();
+                EmployeeService.EmplpoyeeRequest empRequest = new EmployeeService.EmplpoyeeRequest("TFYTW2HKD", Convert.ToInt32(TextBox1.Text));
+                EmployeeService.EmployeeInfo employeeInfo = client.GetEmployee(empRequest);
+                if (employeeInfo.Type == EmployeeService.EmployeeType.FullTimeEmployee)
+                {
+                    txtannual.Text = employeeInfo.AnnualSalary.ToString();
+                    trAnnualsalary.Visible = true;
+                    trhrpay.Visible = false;
+                    trhourworked.Visible = false;
+                }
+                else
+                {
+                    txthrpay.Text = employeeInfo.HourlyPay.ToString();
+                    txthourworked.Text = employeeInfo.HoursWorked.ToString();
+                    trAnnualsalary.Visible = false;
+                    trhrpay.Visible = true;
+                    trhourworked.Visible = true;
+                }
+                ddlEmployeeType.SelectedValue = ((int)employeeInfo.Type).ToString();
+                TextBox2.Text = employeeInfo.Name;
+                TextBox3.Text = employeeInfo.Gender;
+                TextBox4.Text = employeeInfo.DOB.ToShortDateString();
+                Label5.Text = "Employee Retrieved";
+            }
+            catch (FaultException faultException)
+            {
+                Label5.Text = faultException.Message;
+            }
+        }
+
+        protected void btnSaveEmp_Click(object sender, EventArgs e)
+        {
+            EmployeeService.IEmployeeService client =
+              new EmployeeService.EmployeeServiceClient();
+            EmployeeService.EmployeeInfo employeeInfo = new EmployeeService.EmployeeInfo();
+            employeeInfo.Id = 1;
+            employeeInfo.Name = TextBox2.Text;
+            employeeInfo.Gender = TextBox3.Text;
+            employeeInfo.DOB = Convert.ToDateTime(TextBox4.Text);
+            if (ddlEmployeeType.SelectedValue != "-1")
+            {
+                if ((EmployeeService.EmployeeType)Convert.ToInt32(ddlEmployeeType.SelectedValue)
+                == EmployeeService.EmployeeType.FullTimeEmployee)
+                {
+
+                    employeeInfo.Type = EmployeeService.EmployeeType.FullTimeEmployee;
+                    employeeInfo.AnnualSalary = Convert.ToInt32(txtannual.Text);
+                    client.SaveEmployee(employeeInfo);
+                    Label5.Text = "Full Time Employee Saved";
+
+                }
+                else if ((EmployeeService.EmployeeType)Convert.ToInt32(ddlEmployeeType.SelectedValue)
+                    == EmployeeService.EmployeeType.PartTimeEmployee)
+                {
+                    employeeInfo.Type = EmployeeService.EmployeeType.PartTimeEmployee;
+                    employeeInfo.HourlyPay = Convert.ToInt32(txthrpay.Text);
+                    employeeInfo.HoursWorked = Convert.ToInt32(txthourworked.Text);
+                    client.SaveEmployee(employeeInfo);
+                    Label5.Text = "Part Time Employee Saved";
+                }
+
+            }
+
+            else
+            {
+                Label5.Text = "Select Employee Type";
+            }
+
+
+
+
+
+
+        }
+
+        protected void ddlEmployeeType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ddlEmployeeType.SelectedValue == "-1")
+            {
+                trAnnualsalary.Visible = false;
+                trhrpay.Visible = false;
+                trhourworked.Visible = false;
+            }
+            else if (ddlEmployeeType.SelectedValue == "1")
+            {
+                trAnnualsalary.Visible = true;
+                trhrpay.Visible = false;
+                trhourworked.Visible = false;
+            }
+            else
+            {
+                trAnnualsalary.Visible = false;
+                trhrpay.Visible = true;
+                trhourworked.Visible = true;
+            }
+        }
+    }
+}
